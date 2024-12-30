af6ddd2b332ba8dbb617251b3d411f2e5399162b
MartinYordanov374 <martinyordanov284@gmail.com>
2024-12-31 01:37:44 +0200

    Add server port to the docker compose environment variables

--- src/BackEnd/Server/Server.js
+++ src/BackEnd/Server/Server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const connect_to_db = require('../Mongo/Mongoose/Mongoose')
 
-
 const start_server = async() => {
     const app = express()
 
     await connect_to_db()
     
-    app.listen(5000, () => {
-        console.log('express listening on port 8000')
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`express listening on port ${process.env.SERVER_PORT}`)
     })
 }
 
