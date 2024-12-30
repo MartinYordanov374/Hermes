e77f387e745148f3feea875d6abc1f43a4ff93f1
MartinYordanov374 <martinyordanov284@gmail.com>
2024-12-31 01:34:07 +0200

    Backend container now successfully connects with the mongo container

---
+++ src/BackEnd/Mongo/Mongoose/Mongoose.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.MONGO_URI
+
+module.exports = async() => {
+    try{
+        
+        await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Successfully connected with the database')
+
+        mongoose.connection.on('error', (err) => {
+            console.log('A database error occured')
+            console.log(err)
+        })
+    }
+    catch(err)
+    {
+        console.log('An error occured while attempting to connect to the database.')
+        console.log(err)
+        process.exit(1)
+    }
+}
--- src/BackEnd/Server/Server.js
+++ src/BackEnd/Server/Server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const connect_to_db = require('../Mongo/Mongoose/Mongoose')
 
-const app = express()
 
-app.listen(8000, () => {
-    console.log('express listening on port 8000')
-})
+const start_server = async() => {
+    const app = express()
+
+    await connect_to_db()
+    
+    app.listen(5000, () => {
+        console.log('express listening on port 8000')
+    })
+}
+
+start_server()
