49038af5aa600084c0a154497f1c31ab8b947cc2
MartinYordanov374 <martinyordanov284@gmail.com>
2025-01-02 19:41:17 +0200

    Add register user service

---
+++ src/BackEnd/Services/Users/RegisterUser.js
@@ -0,0 +1,29 @@
+const salt_rounds = 9
+
+const bcrypt = require('bcrypt')
+
+const UserModel = require('../../Mongo/Schemas/User')
+
+const RegisterUser = async (username, password, confirmationPassword) => {
+    //TODO: Add password regex to enforce stronger passwords
+    //TODO: Check if user already exists
+    if (password !== confirmationPassword) 
+    {
+      return { status: 403, message: "Passwords don't match" }
+    } 
+    else 
+    {
+      const hashedPassword = await bcrypt.hash(password, salt_rounds);
+  
+      const newUser = await UserModel({
+        Username: username,
+        Password: hashedPassword,
+      });
+
+      await newUser.save();
+  
+      return { status: 200, message: "User registered successfully!" }
+    }
+  };
+  
+module.exports = {RegisterUser}
