e3b593c473ba78811dd17817572a1d3a3dbf665c
MartinYordanov374 <martinyordanov284@gmail.com>
2025-01-02 23:44:45 +0200

    Remove useless files from contact services

--- src/BackEnd/Services/Users/ChangePassword.js
+++ src/BackEnd/Services/Users/ChangePassword.js
@@ -2,8 +2,7 @@ const UserModel = require('../../Mongo/Schemas/User')
 const bcrypt = require('bcrypt')
 const salt_rounds = 10 //process.env.SALT_ROUNDS
 const ChangePassword = async(username, newPassword) => {
-    //TODO: Add password validations, check if user exists before updating, 
-    // check if the user requesting UPDATE is the owner of the profile(session)
+    //TODO: Add password validations, check if user exists before updating
     let updatedUser = await UserModel.findOne({Username: username.toLowerCase()})
     updatedUser.Password = await bcrypt.hash(newPassword, salt_rounds)
     await updatedUser.save()
